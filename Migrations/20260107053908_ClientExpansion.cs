62eb25a523b21055968ea4c19956990f7522809f
Regan Macnamara 20069849 <Reganmac3@gmail.com>
2026-01-07 16:39:51 +1100

    Client Details Migration

---
+++ Migrations/20260107053908_ClientExpansion.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceAutomationAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClientExpansion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AddressLine1",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressLine2",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClientEmail",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClientMobile",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClientPhone",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostCode",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddressLine1",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "AddressLine2",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "ClientEmail",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "ClientMobile",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "ClientPhone",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "PostCode",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Clients");
+        }
+    }
+}
