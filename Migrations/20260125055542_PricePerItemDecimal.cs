560d434d906eab9a0f07ea5ce145991b73fcbd3b
Regan Macnamara 20069849 <Reganmac3@gmail.com>
2026-01-26 11:26:26 +1100

    TransactionItem PricePerItem int => decimal

---
+++ Migrations/20260125055542_PricePerItemDecimal.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceAutomationAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class PricePerItemDecimal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PricePerItem",
+                table: "TransactionItems",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PricePerItem",
+                table: "TransactionItems",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
