0280bd5cbb033bb65f5d697094c0f721691b255a
Regan Macnamara 20069849 <Reganmac3@gmail.com>
2026-02-07 16:12:50 +1100

    API Client Deletion Validation

--- UseCases/Clients/DeleteClient/DeleteClientHandler.cs
+++ UseCases/Clients/DeleteClient/DeleteClientHandler.cs
@@ -13,6 +13,11 @@ namespace BusinessManagementAPI.UseCases.Clients.DeleteClient
 
             if (_Client is not null)
             {
+                var _ClientTransactions = m_Context.Transactions.Where(t => t.ClientID == _Client.ClientID);
+
+                if (_ClientTransactions.Count() > 0)
+                    throw new Exception($"{_Client.ClientName} has Transactions in the system and cannot be deleted.");
+
                 m_Context.Clients.Remove(_Client);
                 await m_Context.SaveChangesAsync();
             }
