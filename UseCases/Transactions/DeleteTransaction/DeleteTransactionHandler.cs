57384edec2c4673a3c847edf182e8f073f60c9ad
Regan Macnamara 20069849 <Reganmac3@gmail.com>
2026-02-10 23:14:00 +1100

    Delete Transaction Removes Transaction Items, Fails if Allocated Too

--- UseCases/Transactions/DeleteTransaction/DeleteTransactionHandler.cs
+++ UseCases/Transactions/DeleteTransaction/DeleteTransactionHandler.cs
@@ -13,6 +13,15 @@ namespace BusinessManagementAPI.UseCases.Transactions.DeleteTransaction
 
             if (_Transaction is not null)
             {
+                var _TransactionAllocations = m_Context.TransactionAllocations.Where(ta => ta.AllocatingID == _Transaction.TransactionID || ta.RecievingID == _Transaction.TransactionID);
+
+                if (_TransactionAllocations.Any())
+                    return Results.BadRequest("Cannot delete a Transaction that has allocations against it.");
+
+                var _TransactionItems = m_Context.TransactionItems.Where(t => t.TransactionID == _Transaction.TransactionID);
+                foreach (var _Item in _TransactionItems)
+                    m_Context.TransactionItems.Remove(_Item);
+
                 m_Context.Transactions.Remove(_Transaction);
                 await m_Context.SaveChangesAsync();
                 return Results.NoContent();
