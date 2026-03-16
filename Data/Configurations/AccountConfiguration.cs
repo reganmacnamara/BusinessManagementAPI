aa0d90202d37ee436346a5a1c09eb7dad893899c
Regan Macnamara 20069849 <Reganmac3@gmail.com>
2026-03-16 21:33:28 +1100

    Account Entity/Config

---
+++ Data/Configurations/AccountConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using BusinessManagementAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BusinessManagementAPI.Data.Configurations
+{
+    public class AccountConfiguration : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.HasKey(c => c.AccountID);
+
+            builder.Property(c => c.Email)
+                .IsRequired();
+
+            builder.Property(c => c.Password)
+                .IsRequired();
+
+            builder.Property(c => c.IsActive)
+                .HasDefaultValue(true)
+                .IsRequired();
+
+            builder.Property(c => c.CreatedDate)
+                .HasDefaultValueSql("GETDATE()")
+                .IsRequired();
+
+            builder.Property(c => c.LastLoginDate);
+        }
+    }
+}
