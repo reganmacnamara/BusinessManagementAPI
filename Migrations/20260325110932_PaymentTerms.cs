a0cb02f9c1090e672c282f8eb28cb8937a71b471
Regan Macnamara 20069849 <Reganmac3@gmail.com>
2026-03-25 22:42:17 +1100

    Payment Term Migration

---
+++ Migrations/20260325110932_PaymentTerms.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceAutomationAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaymentTerms : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "PaymentTermID",
+                table: "Clients",
+                type: "bigint",
+                nullable: true,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateTable(
+                name: "PaymentTerms",
+                columns: table => new
+                {
+                    PaymentTermID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PaymentTermName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Value = table.Column<int>(type: "int", nullable: false),
+                    Unit = table.Column<int>(type: "int", nullable: false),
+                    IsEndOf = table.Column<bool>(type: "bit", nullable: false),
+                    IsStartingNext = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentTerms", x => x.PaymentTermID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_PaymentTermID",
+                table: "Clients",
+                column: "PaymentTermID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Clients_PaymentTerms_PaymentTermID",
+                table: "Clients",
+                column: "PaymentTermID",
+                principalTable: "PaymentTerms",
+                principalColumn: "PaymentTermID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Clients_PaymentTerms_PaymentTermID",
+                table: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "PaymentTerms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Clients_PaymentTermID",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentTermID",
+                table: "Clients");
+        }
+    }
+}
