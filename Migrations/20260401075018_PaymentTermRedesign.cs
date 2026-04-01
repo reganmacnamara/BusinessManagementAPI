f226368208a8cb4f79cc037ea1335af8c123cc0b
Regan Macnamara 20069849 <Reganmac3@gmail.com>
2026-04-01 18:57:15 +1100

    PaymentTerm Redesign Migration

---
+++ Migrations/20260401075018_PaymentTermRedesign.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceAutomationAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaymentTermRedesign : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Value",
+                table: "PaymentTerms",
+                newName: "Months");
+
+            migrationBuilder.RenameColumn(
+                name: "Unit",
+                table: "PaymentTerms",
+                newName: "Days");
+
+            migrationBuilder.RenameColumn(
+                name: "IsStartingNext",
+                table: "PaymentTerms",
+                newName: "OffsetFirst");
+
+            migrationBuilder.RenameColumn(
+                name: "IsEndOf",
+                table: "PaymentTerms",
+                newName: "EndOfMonth");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DayOfMonth",
+                table: "PaymentTerms",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DayOfMonth",
+                table: "PaymentTerms");
+
+            migrationBuilder.RenameColumn(
+                name: "OffsetFirst",
+                table: "PaymentTerms",
+                newName: "IsStartingNext");
+
+            migrationBuilder.RenameColumn(
+                name: "Months",
+                table: "PaymentTerms",
+                newName: "Value");
+
+            migrationBuilder.RenameColumn(
+                name: "EndOfMonth",
+                table: "PaymentTerms",
+                newName: "IsEndOf");
+
+            migrationBuilder.RenameColumn(
+                name: "Days",
+                table: "PaymentTerms",
+                newName: "Unit");
+        }
+    }
+}
